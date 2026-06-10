a2a19a8b1fba0b50e8792bc089725c8c406ef1de
hdpasso29-png <hdpasso29@gmail.com>
2026-06-10 21:08:41 +0300

    fix

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // Definiert 'src' als die Wurzel deines Frontend-Projekts
   root: 'src',
+  // Sagt Vite, dass der öffentliche Ordner eine Ebene höher liegt
+  publicDir: '../public',
   // Bestimmt, wo die fertigen Produktionsdateien landen sollen
   build: {
     outDir: '../dist', // Geht eine Ebene höher, um 'dist' im Hauptverzeichnis zu erstellen
